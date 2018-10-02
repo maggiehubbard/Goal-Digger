af54c94edd5d913fd0b26c20fd93cbbb96f99c2c
Maggie Hubbard <M8065@caidan.local>
2018-10-02 16:17:37 -0400

    added crud functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routerConfig';
+imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
